c6daba33b21bc1fdeeae1414fb74252474a9c4a1
chenzhengen <zhg.chen@muheda.com>
2018-06-11 15:05:28 +0800

    提交修改hello world!

---
+++ webpack.js
@@ -0,0 +1,32 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode:"development",
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      //jsx装载器
+      {
+        test: /\.jsx|js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+        template: './src/index.html', //模板路径
+        // hash: true,
+        minify: {
+            removeAttributeQuotes: true // 移除属性的引号
+        },
+        inject: true
+    }),
+  ]
+};
